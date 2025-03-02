ff13c59734570223eed6e8a8dbc1dde3c8c97de8
Alireza <alirezam.p90000@gmail.com>
2025-03-02 08:23:13 -0800

    Create New Address Api Completed

--- app/Http/Controllers/Api/AddressController.php
+++ app/Http/Controllers/Api/AddressController.php
@@ -3,18 +3,67 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Contracts\Routing\ResponseFactory;
-use Illuminate\Foundation\Application;
+use App\Http\Requests\Api\AddressRequest;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class AddressController extends Controller
 {
     /**
-     * @return Application|Response|\Illuminate\Contracts\Foundation\Application|ResponseFactory
+     * Display a listing of the resource.
      */
     public function index(): \Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
     {
         return apiResponse(auth()->user()->addresses->toArray());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(AddressRequest $request): \Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
+        $data = $request->only([
+            'name',
+            'address',
+            'latitude',
+            'longitude'
+        ]);
+
+        /* Create Address */
+        $address = auth()->user()->addresses()->create($data);
+        /* Create Address */
+
+        return apiResponse($address->toArray(), 'Address created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }

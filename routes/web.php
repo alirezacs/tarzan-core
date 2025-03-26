ce601d4685d4a8771a55189a586e4d64b727276c
Alireza <alirezam.p90000@gmail.com>
2025-03-26 09:30:35 -0700

    Install And Config Tailwind

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use App\Models\BasketItem;
 use App\Models\RequestType;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\BasketController;
 
-/* Basket Routes */
-Route::get('basket/verify', [BasketController::class, 'verify'])->name('basket.verify');
-/* Basket Routes */
+Route::view('/', 'welcome');
